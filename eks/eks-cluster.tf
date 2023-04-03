263b204deb082c4d657b220e24c43f098b217183
RAJUKUMAR SUDDALA <97968060+SRK-RAJU@users.noreply.github.com>
2023-04-03 21:21:13 +0530

    Update eks-cluster.tf

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -14,21 +14,21 @@ module "eks" {
 worker_groups = [
     {
       name                          = "frontend-worker-group"
-      instance_type                 = "t2.small"
+      instance_type                 = "t3.small"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
     },
     {
       name                          = "backend-java-worker-group"
-      instance_type                 = "t2.small"
+      instance_type                 = "t3.small"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
     },
    {
       name                          = "database-worker-group"
-      instance_type                 = "t2.medium"
+      instance_type                 = "t3.medium"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
       asg_desired_capacity          = 1
